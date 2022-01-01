ee311d8e0f2a49b76e93253d5c2f88b55a654846
Ghidorah <jljohnson1454@gmail.com>
2022-01-01 11:03:12 -0600

    added mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -59,7 +59,19 @@ const resolvers = {
                   );
                   return updatedUser;
               }
-          }
+              
+          },
+        removeBook: async (parent, args, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate (
+                    { _id: context.user._id },
+                    { $pull: { savedBooks: { bookId: args.bookId }}},
+                    { new: true }
+                );
+                return updatedUser;
+            }
+
+        }
     }
   };
   
